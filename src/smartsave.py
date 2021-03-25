2ff01ab20836ce2fb63926101539cadfb95d5345
ezhang090 <77946230+ezhang090@users.noreply.github.com>
2021-03-24 21:26:16 -0500

    Initializing Defaults
    Added model and version defaults if none are provided by user. Removed not path/scene warning.

--- src/smartsave.py
+++ src/smartsave.py
@@ -25,7 +25,9 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.setMaximumHeight(200)
         self.setWindowFlags(self.windowFlags() ^
                            QtCore.Qt.WindowContextHelpButtonHint)
+        self.scenefile = SceneFile()
         self.create_ui()
+ #       self.create_connections()
 
     def create_ui(self):
         self.title_lbl = QtWidgets.QLabel("Smart Save")
@@ -51,14 +53,14 @@ class SmartSaveUI(QtWidgets.QDialog):
 
     def _create_filename_ui(self):
         layout = self._create_filename_headers()
-        self.descriptor_le = QtWidgets.QLineEdit("main")
+        self.descriptor_le = QtWidgets.QLineEdit(self.scenefile.descriptor)
         self.descriptor_le.setMinimumWidth(100)
-        self.task_le = QtWidgets.QLineEdit("model")
+        self.task_le = QtWidgets.QLineEdit(self.scenefile.task)
         self.task_le.setFixedWidth(50)
         self.ver_sbx = QtWidgets.QSpinBox()
         self.ver_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
         self.ver_sbx.setFixedWidth(50)
-        self.ver_sbx.setValue(1)
+        self.ver_sbx.setValue(self.scenefile.ver)
         self.ext_lbl = QtWidgets.QLabel(".ma")
         layout.addWidget(self.descriptor_le, 1, 0)
         layout.addWidget(QtWidgets.QLabel("_"), 1, 1)
@@ -95,17 +97,17 @@ class SmartSaveUI(QtWidgets.QDialog):
 class SceneFile(object):
     """An abstract representation of a scene file."""
     def __init__(self, path=None):
-        self.folder_path = Path()
+        self.folder_path = Path(cmds.workspace(query=True,
+                                               rootDirectory=True)) / "scenes"
         self.descriptor = 'main'
-        self.task = None
+        self.task = 'model'
         self.ver = 1
         self.ext = '.ma'
         scene = pmc.system.sceneName()
         if not path and scene:
             path = scene
         if not path and not scene:
-            log.warning("Unable to initialize SceneFile object from "
-                        "a new scene, please specify a path")
+            log.info("Initialize with default properties.")
             return
         self._init_from_path(path)
 
@@ -169,3 +171,7 @@ class SceneFile(object):
         """
         self.ver = self.next_avail_ver()
         self.save()
+
+   # def create_connections(self):
+   #     """connect our widget signals to slots"""
+   #     self.cancel_btn.clicked.connect(self.cancel)
