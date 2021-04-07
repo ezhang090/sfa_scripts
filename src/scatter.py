3b90173655b48db866e319f5202f36439413a0ee
ezhang090 <77946230+ezhang090@users.noreply.github.com>
2021-04-07 16:09:43 -0500

    Scatter Button and Line Editors for Rotation
    Added input boxes for x,y,z rotation min and max. Added scatter button at bottom of UI.

--- src/scatter.py
+++ src/scatter.py
@@ -34,13 +34,21 @@ class ScatterUI(QtWidgets.QDialog):
         self.title_lbl.setStyleSheet("font: bold 20px")
         self.source_dd_lay = self._create_source_dd()
         self.destination_dd_lay = self._create_destination_dd()
-        self.scale_ui = self._create_scale_ui()
+        self.input_ui = self._create_input_ui()
+        self.button_lay = self._create_button_ui()
         self.main_lay = QtWidgets.QVBoxLayout()
         self.main_lay.addWidget(self.title_lbl)
         self.main_lay.addLayout(self.source_dd_lay)
         self.main_lay.addLayout(self.destination_dd_lay)
-        self.main_lay.addLayout(self.scale_ui)
+        self.main_lay.addLayout(self.input_ui)
+        self.main_lay.addLayout(self.button_lay)
         self.setLayout(self.main_lay)
+
+    def _create_button_ui(self):
+        self.scatter_btn = QtWidgets.QPushButton("Scatter")
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.scatter_btn)
+        return layout
 
     def _create_source_dd(self):
         self.source_dd_lbl = QtWidgets.QLabel("Select Source Object")
@@ -75,22 +83,72 @@ class ScatterUI(QtWidgets.QDialog):
     def destination_index_changed(self):
         cmds.select(self.destination_dd.currentText(), toggle=True)
 
-    def _create_scale_ui(self):
-        layout = self._create_scale_headers()
-        self.xscale_lbl = QtWidgets.QLabel("x Scale")
-        self.yscale_lbl = QtWidgets.QLabel("y Scale")
-        self.zscale_lbl = QtWidgets.QLabel("z Scale")
-        layout.addWidget(self.xscale_lbl, 1, 0)
-        layout.addWidget(self.yscale_lbl, 2, 0)
-        layout.addWidget(self.zscale_lbl, 3, 0)
+    def _create_input_ui(self):
+        layout = self._create_input_headers()
+        self._min_input_ui(layout)
+        self._max_input_ui(layout)
         return layout
 
-    def _create_scale_headers(self):
+    def _min_input_ui(self, layout):
+        self.xscale_min_le = QtWidgets.QLineEdit()
+        self.xscale_min_le.setFixedWidth(100)
+        self.yscale_min_le = QtWidgets.QLineEdit()
+        self.yscale_min_le.setFixedWidth(100)
+        self.zscale_min_le = QtWidgets.QLineEdit()
+        self.zscale_min_le.setFixedWidth(100)
+        self.xrotate_min_le = QtWidgets.QLineEdit()
+        self.xrotate_min_le.setFixedWidth(100)
+        self.yrotate_min_le = QtWidgets.QLineEdit()
+        self.yrotate_min_le.setFixedWidth(100)
+        self.zrotate_min_le = QtWidgets.QLineEdit()
+        self.zrotate_min_le.setFixedWidth(100)
+        layout.addWidget(self.xscale_min_le, 1, 1)
+        layout.addWidget(self.yscale_min_le, 2, 1)
+        layout.addWidget(self.zscale_min_le, 3, 1)
+        layout.addWidget(self.xrotate_min_le, 4, 1)
+        layout.addWidget(self.yrotate_min_le, 5, 1)
+        layout.addWidget(self.zrotate_min_le, 6, 1)
+        return layout
+
+    def _max_input_ui(self, layout):
+        self.xscale_max_le = QtWidgets.QLineEdit()
+        self.xscale_max_le.setFixedWidth(100)
+        self.yscale_max_le = QtWidgets.QLineEdit()
+        self.yscale_max_le.setFixedWidth(100)
+        self.zscale_max_le = QtWidgets.QLineEdit()
+        self.zscale_max_le.setFixedWidth(100)
+        self.xrotate_max_le = QtWidgets.QLineEdit()
+        self.xrotate_max_le.setFixedWidth(100)
+        self.yrotate_max_le = QtWidgets.QLineEdit()
+        self.yrotate_max_le.setFixedWidth(100)
+        self.zrotate_max_le = QtWidgets.QLineEdit()
+        self.zrotate_max_le.setFixedWidth(100)
+        layout.addWidget(self.xscale_max_le, 1, 2)
+        layout.addWidget(self.yscale_max_le, 2, 2)
+        layout.addWidget(self.zscale_max_le, 3, 2)
+        layout.addWidget(self.xrotate_max_le, 4, 2)
+        layout.addWidget(self.yrotate_max_le, 5, 2)
+        layout.addWidget(self.zrotate_max_le, 6, 2)
+        return layout
+
+    def _create_input_headers(self):
         self.min_lbl = QtWidgets.QLabel("Minimum")
         self.min_lbl.setStyleSheet("font: bold")
         self.max_lbl = QtWidgets.QLabel("Maximum")
         self.max_lbl.setStyleSheet("font: bold")
+        self.xscale_lbl = QtWidgets.QLabel("x Scale")
+        self.yscale_lbl = QtWidgets.QLabel("y Scale")
+        self.zscale_lbl = QtWidgets.QLabel("z Scale")
+        self.xrotate_lbl = QtWidgets.QLabel("x Rotate")
+        self.yrotate_lbl = QtWidgets.QLabel("y Rotate")
+        self.zrotate_lbl = QtWidgets.QLabel("z Rotate")
         layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.xscale_lbl, 1, 0)
+        layout.addWidget(self.yscale_lbl, 2, 0)
+        layout.addWidget(self.zscale_lbl, 3, 0)
+        layout.addWidget(self.xrotate_lbl, 4, 0)
+        layout.addWidget(self.yrotate_lbl, 5, 0)
+        layout.addWidget(self.zrotate_lbl, 6, 0)
         layout.addWidget(self.min_lbl, 0, 1)
         layout.addWidget(self.max_lbl, 0, 2)
         return layout
